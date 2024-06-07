f192dff26f4220dcec141ee2e87efe517184c040
s28581 <s28581@pjwstk.edu.pl>
2024-06-08 01:58:33 +0200

    Data Context done

--- APBDProbnyKol2/APBDProbnyKol2/Data/DataContext.cs
+++ APBDProbnyKol2/APBDProbnyKol2/Data/DataContext.cs
@@ -1,8 +1,32 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using APBDProbnyKol2.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace APBDProbnyKol2.Data;
 
 public class DataContext : DbContext
 {
-    
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Album> Albumy { get; set; }
+    public DbSet<Muzyk> Muzyka { get; set; }
+    public DbSet<Utwor> Utwory { get; set; }
+    public DbSet<WykonawcaUtworu> WykonawcaUtworow { get; set; }
+    public DbSet<Wytwornia> Wytwornie { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<WykonawcaUtworu>()
+            .HasKey(wu => new { wu.IdMuzyk, wu.IdUtwor });
+        modelBuilder.Entity<WykonawcaUtworu>()
+            .HasOne(w => w.Muzyk)
+            .WithMany(wu => wu.WykonawcaUtworu)
+            .HasForeignKey(w => w.IdMuzyk);
+        modelBuilder.Entity<WykonawcaUtworu>()
+            .HasOne(u => u.Utwor)
+            .WithMany(wu => wu.WykonawcaUtworu)
+            .HasForeignKey(u => u.IdUtwor);
+    }
 }
